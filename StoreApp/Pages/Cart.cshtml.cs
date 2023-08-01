9a1ee8a72881ecd86281b14e979892fb83f685e0
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-08-01 14:55:13 +0300

    Cart widget updated

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -10,25 +10,27 @@ public class CartModel : PageModel
 {
     private IServiceManager _serviceManager;
     public Cart Cart { get; set; }
-    
+    public string ReturnUrl { get; set; } = "/";
     public CartModel(IServiceManager serviceManager, Cart cart)
     {
         _serviceManager = serviceManager;
         Cart = cart;
     }
 
-    public void OnGet()
+    public void OnGet(string returnUrl)
     {
+        ReturnUrl = returnUrl ?? "/";
     }
 
-    public IActionResult OnPost(int id)
+    public IActionResult OnPost(int id, string returnUrl)
     {
+        
         Product product = _serviceManager.ProductService.GetById(id,false);
         if (product is not null)
         {
             Cart.AddLine(product, 1);
         }
-        return Page();
+        return RedirectToPage(new {returnUrl = returnUrl});
     }
 
     public void OnPostRemove(int id)
